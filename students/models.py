cab59c17e4b4113cbedc78d0b8dc6093f7f90a99
Usman4862 <um4862@gmail.com>
2023-02-26 13:55:11 -0800

    remove dunder method

--- students/models.py
+++ students/models.py
@@ -21,11 +21,6 @@ class Student(models.Model):
     join = models.DateField(null=True, blank=True, auto_now_add=True)
     pass_matric = models.BooleanField(default=True)
     
-    def __str__(self):
-        return f"{self.roll_number} - {self.first_name} - {self.last_name} - {self.join} - {self.student_class}"
-
-
-    
 
 
 
