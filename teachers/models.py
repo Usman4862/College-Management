29e2f1e196112ec3332545dfff8d456ee91c321e
Usman4862 <um4862@gmail.com>
2023-02-26 13:59:30 -0800

    add new models and modify choices names and remove dunder methods

--- teachers/models.py
+++ teachers/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 # Create your models here.
-CLASS = {
+TEACHER_CLASS = {
     ("ICS-P1", "ICS-PART1"),
     ("ICS-P2", "ICS-PART2"),
     ("M-P1", "MEDICAL-PART1"),
@@ -12,20 +12,29 @@ class Teacher(models.Model):
     first_name = models.CharField(max_length= 30, null=False, blank=False)
     last_name = models.CharField(max_length= 30, null=False, blank=False)
     date_of_birth = models.DateTimeField(auto_now_add=False, auto_now=False, null=True, blank=True)
-    teacher_class = models.CharField(max_length=10, choices=CLASS)
+    teacher_class = models.CharField(max_length=10, choices=TEACHER_CLASS)
     joining_date = models.DateField(auto_now_add=True)
     contact_number = models.PositiveBigIntegerField(blank=False, null=False)
     still_joined = models.BooleanField(default=True)
-    teacher_id = models.BigAutoField(primary_key=True)
-    def __str__(self):
-        return f"{self.first_name} -- {self.last_name} -- {self.joining_date}"
-    
-    
-    
-
-class Salary:
-    pass
+    teacher_id = models.BigAutoField(primary_key=True)    
 
 
-class Billing_history:
-    pass
+SALARY_CHOICE = {
+    ("N", "NO"),
+    ("Y", "YES"),
+}
+class Salary(models.Model):
+    weekly_hours = models.SmallIntegerField(default=0, verbose_name="Weekly Duty Hours")
+    paying_in_hours = models.SmallIntegerField(default=0)
+    paying_amount = models.PositiveIntegerField(default=0, verbose_name='Salary')
+    last_paid = models.DateField(auto_now_add=True, auto_now=False)
+    current_month_amount = models.CharField(max_length=2, verbose_name='Is Current Month Salary Payed?', choices=SALARY_CHOICE, default="YES")
+    class meta:
+        abstract = True
+        verbose_name = "Salary"
+        verbose_name_plural = "Salary"
+        
+
+    
+
+        
