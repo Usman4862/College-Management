2ae64f3cf6f0805f98d01d021ff684612ecaec07
Usman4862 <um4862@gmail.com>
2023-02-26 13:54:26 -0800

    modify student admin with Decorator register method

--- students/admin.py
+++ students/admin.py
@@ -3,4 +3,14 @@ from students.models import Student
 
 # Register your models here.
 
-admin.site.register(Student)
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display= ['roll_number', 'first_name', 'last_name', 'join', 'student_class']
+    date_hierarchy= 'join'
+    save_as= True
+    save_as_continue= True
+    save_on_top= True
+    save_as_bottom= False
+    search_fields= ['first_name', 'last_name', 'student_class', 'roll_number']
+    preserve_filters= True
+    exclude= ['join', 'date_of_birth']
