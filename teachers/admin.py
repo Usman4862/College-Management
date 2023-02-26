90ae3b3c7f4b0a57422612be6c99c521ce3cd0a7
Usman4862 <um4862@gmail.com>
2023-02-26 13:58:04 -0800

    register teacher model at admin

--- teachers/admin.py
+++ teachers/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from teachers.models import Teacher
+from teachers.models import Teacher, Salary
 # Register your models here.
 
-admin.site.register(Teacher)
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display= ['first_name', 'last_name', 'joining_date']
+    date_hierarchy = 'joining_date'
+    save_as= True
+    save_on_top= True
+    save_as_continue= True
+    save_as_bottom=False
+    search_fields= ['first_name', 'last_name', 'teacher_class', 'contact_number', 'teacher_id']
+    preserve_filters= True
+    exclude= ['still_joined']
+
+@admin.register(Salary)
+class SalaryAdmin(admin.ModelAdmin):
+    list_display= ['paying_amount', 'paying_in_hours', 'current_month_amount', 'last_paid']
+    save_on_top= True
+    search_fields= ['paying_amount']
+    preserver_filters= True
