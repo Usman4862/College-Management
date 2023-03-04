2df96ea1cdc32273b26109f8abb07cf7e67333fb
Silent coder <120396373+Usman4862@users.noreply.github.com>
2023-03-04 19:41:21 +0500

    add urls

--- teachers/urls.py
+++ teachers/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('teacher/list/', views.teachers_list, name='teacher-list')
+    path('teacher/list', views.teachers_list, name='teacher-list'),
+    path('teacher/add', views.add_teacher, name='teacher-add'),
+    path('teacher/delete/<int:id>', views.delete_teacher, name='teacher-delete'),
+    path('teacher/view/<int:id>', views.view_teacher, name='teacher-view'),
 ]
